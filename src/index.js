c2035ebce38cac031a2f4237cdef3d983fadf1f1
Leroy G <liguan698@gmail.com>
2023-04-07 21:20:25 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTasks(e.target['new-task-description'].value)
+    form.reset()
+  })
+})
+
+function createTasks (task) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  li.textContent = `${task}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
